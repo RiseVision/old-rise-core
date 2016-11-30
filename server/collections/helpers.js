1c8d971769d427d1b7a10114d5855c7bad0b1738
Nathanael Burchill <nathan.burchill@dontpanic.consulting>
2016-11-30 18:56:32 -0500

    Added the ability to use triggers on database activity.

--- server/collections/helpers.js
+++ server/collections/helpers.js
@@ -1,8 +1,13 @@
 import { Mongo } from 'meteor/mongo';
 
-export const defineCollection = function(name, schema){
+export const defineCollection = function(name, schema, triggers){
     let collection = new Mongo.Collection(name);
     collection.schema = schema;
     collection.attachSchema(schema);
+    if(triggers){
+        triggers.forEach(function(obj){
+            collection[obj.timing][obj.type](obj.func);
+        });
+    }
     return collection;
 }
