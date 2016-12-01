c235a4499e1eb18fe46ead21ad1eab1f154bff12
Nathanael Burchill <nathan.burchill@dontpanic.consulting>
2016-11-30 19:00:30 -0500

    Added functions to the Meteor.methods to allow updating the previousBlock from the collection definition.

--- server/main.js
+++ server/main.js
@@ -4,6 +4,27 @@ import { DB } from './collections.js';
 Meteor.methods({
     getCollectionCount: (collection) => {
         return DB[collection].find().count();
+    },
+    getPreviousId: (collection, id) => {
+        let idQuery = DB[collection].findOne({
+            _id: {
+                $eq: id
+            }
+        });
+        let rowQuery = DB[collection].findOne({
+            rowId: {
+                $eq: idQuery.rowId - 1
+            }
+        });
+        return rowQuery._id;
+    },
+    // The purpose of this function is to update a collection during autovalue generation.
+    updateCollection: (collection, selector, modifier) => {
+        DB[collection].update(selector, modifier, function(error, result){
+            if(error){
+                console.log(error);
+            }
+        });
     }
 });
 
