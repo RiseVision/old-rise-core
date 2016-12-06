0c726c07eb71a69a88357b06ddfa431474661294
[knightfenris] <[richard@dontpanic.consulting]>
2016-12-06 16:23:53 -0500

    moved todos to issues in github

---
+++ server/delegates.js
@@ -0,0 +1,4 @@
+// TODO: Create list of delegates from peers in database that will create blocks and what peer is involved for each round
+// TODO: function to generate the list from the peers and send out this list to all delegates in the delegate list
+// TODO: receive new block forging list, wait for block to be recieved on the list, if no block is recieved in 10 sec, move past that delegate
+// TODO: Update Consensus Limit value when new round starts based on current peer list
