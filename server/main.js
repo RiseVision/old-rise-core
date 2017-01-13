45a9a3ac5e1d22d1e6743591b5928ceccc96464e
Nathanael Burchill <nathan.burchill@dontpanic.consulting>
2017-01-13 15:26:13 -0500

    Trying to do this by hand instead.

--- server/main.js
+++ server/main.js
@@ -1,9 +1,10 @@
 import { Meteor } from "meteor/meteor";
 import { DB } from "./collections.js";
+import { DDP } from "meteor/ddp-client";
 
 let serverIPs = ["192.168.56.101", "192.168.56.102", "192.168.56.103"];
+let myIP = "";
 let servers = [];
-
 Meteor.methods({
     getCollectionCount: (collection) => {
         return DB[collection].find().count();
@@ -41,40 +42,11 @@ Meteor.methods({
 
 Meteor.startup(() => {
     serverIPs.forEach(function(serverIP){
-        servers.push(new ServerSyncClient("http://" + serverIP + ":3000", {
-            onConnect: function() {
-                console.log("connected to master");
-            },
-            onReconnect: function() {
-                console.log("reconnected to master");
-            },
-            beforeSyncDirty: function(count) {
-                console.log("beforeSyncDirty", count);
-            },
-            afterSyncDirty: function(count) {
-                console.log("afterSyncDirty", count);
-            }
-        }));
+        if(myIP !== serverIP){
+            servers.push(DDP.connect("http://" + serverIP + ":3000"));
+        }
     });
     servers.forEach(function(server){
-        server.sync("testingCollection", {
-            onReady: function() {
-                var coll = server.getCollection("testingCollection");
-                console.log("ready", coll.find().count());
-            },
-            beforeSyncUp: function(type, id, doc) {
-                console.log("beforeSyncUp", type, id, doc);
-            },
-            beforeSyncDown: function(type, id, doc) {
-                console.log("beforeSyncDown", type, id, doc);
-            },
-            afterSyncUp: function(type, id, doc) {
-                console.log("afterSyncUp", type, id, doc);
-            },
-            afterSyncDown: function(type, id, doc) {
-                console.log("afterSyncDown", type, id, doc);
-            },
-            args: [Date.now()]
-        });
+        server.subscribe("testingCollection");
     });
 });
